46f87b1badec0cd72dafe4b403b1a20634c6602c
XYmar <xqingliuwu@163.com>
2017-12-05 15:50:22 +0800

    1、模板与框架整体改版。 2、加入嵌套路由实现页面切换。 3、实现切换页面显示对应页面的内容。

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import xy1 from '@/pages/xy1'
+import xy2 from '@/pages/xy2'
 
 Vue.use(Router)
 
 export default new Router({
+  /* eslint-disable */
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      children: [
+        {
+          path: '/xy1', 
+          name: 'xy1',
+          component: xy1
+        },
+        {
+          path: '/xy2', 
+          name: 'xy2',
+          component: xy2
+        }
+      ]
     }
+    
   ]
 })
