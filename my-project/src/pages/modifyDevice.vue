ca7e76422b3564cf926f1e699a39d70d1b9a29c8
XYmar <xqingliuwu@163.com>
2017-12-15 16:27:50 +0800

    完成设备的修改功能（问题：修改页面显示的是全部，未表单验证）

--- my-project/src/pages/modifyDevice.vue
+++ my-project/src/pages/modifyDevice.vue
@@ -21,28 +21,41 @@
           <!-- left column -->
           <div class="span10 with-sidebar">
             <div class="container">
-              <form class="new_user_form inline-input" />
-              <div class="span12 field-box">
+              <form class="new_user_form inline-input"/>
+              <div v-for="(device,index) in devices" :key="index">
+
+                <div class="span12 field-box">
+                <label></label>
+                <input class="span9" type="hidden" name="edit-id" :value="device.id"/>
+                </div>
+
+                <div class="span12 field-box">
                 <label>设备名:</label>
-                <input class="span9" type="text" name="add-name"/>
-              </div>
-
-              <div class="span12 field-box">
-                <label>IP:</label>
-                <input class="span9" type="text" name="add-IP"/>
-              </div>
-
-              <div class="span12 field-box">
-                <label>描述:</label>
-                <input class="span9" type="text" name="add-describle"/>
-              </div>
-
-              <div class="span7 field-box actions">
-              <input type="button" class="btn-glow primary" value="保存" style="width: 100px;" @click="addUser"/>
-              </div>
+                <input class="span9" type="text" name="edit-name" :value="device.name"/>
+                </div>
+                
+                <div class="span12 field-box">
+                  <label>IP:</label>
+                  <input class="span9" type="text" name="edit-ip" :value="device.ip"/>
+                </div>
+                
+                <div class="span12 field-box">
+                  <label>描述:</label>
+                  <input class="span9" type="text" name="edit-des" :value="device.description"/>
+                </div>
+                
+                <div class="span7 field-box actions">
+                <input type="button" class="btn-glow primary" value="保存" style="width: 100px;" @click="editDevice"/>
+                </div>  
+              </div>  
 
               </form>
             </div>
+          </div>
+
+          <hr/>
+          <div>
+              {{devices}}
           </div>
 
 
@@ -53,42 +66,63 @@
 </template>
 
 <script>
-  export default{
-    /* eslint-disable */
-    data(){
-      return{
+export default{
+        /* eslint-disable */
+        data(){
+            return{
+                devices:[]
+            }
+            message: $("input[name='edit-id']").val()
+        },created(){
+            this.$axios.get('devices',{
+                //设置头
+                headers:{
+                    'content-type':'application/x-www-form-urlencoded'
+                },
+                auth: {
+                    username: 'admin',
+                    password: 'admin'
+                }
+            }).then(res=>{
+                this.devices = res.data.data
+            })
+            .catch(err=>{
+                console.log(err);
+            })
 
-      }
-    },
-    methods: {
-      addUser: function (){
-        var qs = require('qs');
-        this.$axios.post('users',qs.stringify({
-          "username": $("input[name='add-name']").val(),
-          "password": $("input[name='add-password']").val()
-        }),{
-          /*params:{  //get请求在第二个位置，post在第三个位置
-           ID:'c02da6e9-a334-4e41-b842-c59eb7d0d3f3'
-           },*/
-          //设置头
-          headers:{
-            'content-type':'application/x-www-form-urlencoded'
-          },
-          auth: {
-            username: 'admin',
-            password: 'admin'
-          }
-        }).then(res=>{
-          //this.users = res.data.data
-          //console.log(res);
-          this.$router.replace({ path: '/xy1'})
-        })
-          .catch(err=>{
-            alert("请重新输入用户名！");
-          })
-      }
+        },
+        methods: {
+            editDevice: function (){
+                var id = $("input[name='edit-id']").val();
+                alert(id);
+                var qs = require('qs');
+                this.$axios.patch('devices/'+id ,qs.stringify({
+                    "id": $("input[name='edit-id']").val(),
+                    "name": $("input[name='edit-name']").val(),
+                    "ip": $("input[name='edit-ip']").val(),
+                    "description": $("input[name='edit-des']").val()
+                }),{
+                    /*params:{  //get请求在第二个位置，post在第三个位置
+                     ID: $("input[name='edit-id']").val()
+                    },*/
+                    //设置头
+                    headers:{
+                        'content-type':'application/x-www-form-urlencoded'
+                    },
+                    auth: {
+                        username: 'admin',
+                        password: 'admin'
+                    }
+                }).then(res=>{
+                    //this.users = res.data.data
+                    //console.log(res);
+                    this.$router.replace({ path: '/devices'})
+                }).catch(err=>{
+                    alert("请重新输入用户名！");
+                })
+            }
+        }
     }
-  }
 </script>
 <style>
 
