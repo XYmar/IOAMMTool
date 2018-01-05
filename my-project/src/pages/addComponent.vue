aaf2f4560e8f9ea072e3c48260f94075f83526da
XYmar <xqingliuwu@163.com>
2018-01-05 09:29:59 +0800

    新增组件版本一（将文件或文件夹分别添加到列表）

--- my-project/src/pages/addComponent.vue
+++ my-project/src/pages/addComponent.vue
@@ -22,54 +22,71 @@
                     <div class="span10 with-sidebar">
                         <div class="container">
                             <form class="new_user_form inline-input" />
-                            <div class="span12 field-box">
-                                <label>组件名:</label>
-                                <input class="span9" type="text" name="add-name"/>
-                            </div>
+                                <div class="span12 field-box">
+                                    <label>组件名:</label>
+                                    <input class="span9" type="text" name="add-name"/>
+                                </div>
 
-                            <div class="span12 field-box">
-                                <label>版本:</label>
-                                <input class="span9" type="text" name="add-version"/>
-                            </div>
+                                <div class="span12 field-box">
+                                    <label>版本:</label>
+                                    <input class="span9" type="text" name="add-version"/>
+                                </div>
 
+                                <div class="span12 field-box">
+                                    <label>大小:</label>
+                                    <input class="span9" type="text" name="add-size"/>
+                                </div>
 
-                            <div class="span12 field-box">
-                                <label>描述信息:</label>
-                                <input class="span9" type="text" name="add-describle"/>
-                            </div>
+                                <div class="span12 field-box">
+                                    <label>描述信息:</label>
+                                    <input class="span9" type="text" name="add-describle"/>
+                                </div>
 
-                            <div class="span12 field-box">
-                                <label>大小:</label>
-                                <input class="span9" type="text" name="add-size"/>
-                            </div>
+                                <div class="span12 field-box">
+                                    <label>上传文件:</label>
+                                    <div class="span9 uploadfile" style="margin-left: -1px;">
+                                        <!-- <ul id="example-1">
+                                            <li v-for="(index,item) in items">
+                                                {{ item.message }}
+                                            </li>
+                                        </ul> -->
+                                        <!-- 列表1  文件夹 -->
+                                        <div id="todo-list-example" class="addli">
+                                            <ul>
+                                                <li v-for="(folder, index) in folders" :key="index">
+                                                    {{folder}}
+                                                </li>
+                                            </ul>
+                                        </div>
 
-                            <div class="span12 field-box">
-                                <label>上传文件:</label>
-                                <div class="span9 uploadfile" style="margin-left: -1px;">
-                                    <!-- <ul id="example-1">
-                                        <li v-for="(index,item) in items">
-                                            {{ item.message }}
-                                        </li>
-                                    </ul> -->
-                                    <div id="todo-list-example" class="addli">
-                                        <!-- <button v-on:click="ss">保存 </button> -->
-                                        <ul>
-                                            <li is="todo-item" v-for="(file, index) in files"  v-text="sv"></li>
-                                        </ul>
-                                    </div>
-                                    <div class="upbtn">
-                                        <!-- <form method="POST" enctype=multipart/form-data> -->
-                                            <input type='file' name="file" webkitdirectory >
-                                            <input type='file' name="file">
-                                            <button v-on:click="ss">upload</button>
-                                        <!-- </form>  -->
+                                        <div class="upbtn">
+                                            <input type='file' name="folderin" id="folderupload" webkitdirectory  @change="getFolder($event)">
+                                                <!-- <input type='file' name="folderin" id="folderupload" webkitdirectory > -->
+                                                <!-- <input type='file' name="file"> -->
+                                                <button v-on:click="folderclick">upload</button>
+                                                <!-- <button v-on:click="folderclick">upload</button> -->
+                                        </div>
+
+                                        <!-- 列表2 文件 -->
+                                        <div id="todo-list-example" class="addli">
+                                            <ul>
+                                                <li v-for="(file, index) in files" :key="index">
+                                                    {{file}}
+                                                </li>
+                                                <!-- <li is="todo-item" v-for="(file, index) in files"  v-text="sv2"></li> -->
+                                            </ul>
+                                        </div>
+                                        
+                                        <div class="upbtn">
+                                                <input type='file' name="filein" id="fileupload"  @change="getFile($event)">
+                                                <button v-on:click="fileclick">upload</button>
+                                        </div> 
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="span7 field-box actions">
-                                <input type="button" class="btn-glow primary" value="创建" style="width: 100px;" @click="addUser"/>
-                            </div>
+                                <div class="span7 field-box actions">
+                                    <input type="button" class="btn-glow primary" value="添加组件" style="width: 100px;" @click="addComp($event)"/>
+                                </div>
                             </form>
                         </div>
                     </div>
@@ -78,6 +95,20 @@
                 </div>
             </div>
         </div>
+
+
+        <hr/>
+        <div>
+            {{folders}}
+        </div>
+        <hr/>
+        <div>
+            {{files}}
+        </div>
+        <hr/>
+        <div>
+            {{allArr}}
+        </div>
     </div>
 </template>
 
@@ -85,62 +116,133 @@
 /* eslint-disable */
 import Vue from 'vue'
 
-let files = [];
+let folders = [];   //上传文件夹的数组
+let files = [];     //上传文件的数组
+let allArr = [];    //上传文件夹和文件的总数组
 let sv = null;
-Vue.component('todo-item', {
+let sv1 = null;
+/*Vue.component('todo-item', {
   template: `
             <li v-on:click="$emit('click')">
                 {{ text }}
             </li>`,
             props: ['text']
-})
+})*/
 
-    export default{
+export default {
 
-        data(){
-            return{
+        data:function() {
+            return {
+                name:'',
+                version:'',
+                folders: [
+                    
+                ],
                 files: [
                     
                 ],
-                sv: ''
+                allArr: [
+
+                ],
+                sv1: '',
+                sv2: ''
             }
         },
         methods: {
-            addUser: function (){
-                var qs = require('qs');
-                this.$axios.post('components',qs.stringify({
-                        "componentfile": $("input[name='file']").val()
-                    }),{
-                
-                auth: {
-                    username: 'admin',
-                    password: 'admin'
-                }
-            }).then(res=>{
-                
-            })
-            .catch(err=>{
-                alert("失败！");
-            })
+            getFolder(event) {
+                this.sv1 = event.target.files[0];
+                console.log(this.sv1.length);
+                console.log(this.sv1);
+
+                /*this.sv1 = $("input[name='file']").val();
+                if(this.sv1.length != 0){
+                    this.folders.push(this.sv1);
+
+                    this.allArr.push(this.sv1);
+                    var obj = document.getElementById('fileupload') ; 
+                    obj.outerHTML=obj.outerHTML;
+                }else{
+                    alert("请选择文件夹");
+                }*/
             },
-            ss: function() {
+            /*getFile(event) {
+                this.allArr = event.target.files[0];
+                console.log(this.allArr.length);
+                console.log(this.allArr);
+            },*/
+            folderclick(event) {
                 //alert(this.sv);
-                this.sv = $("input[name='file']").val();
-                this.files.push(this.sv)
+                //this.sv1 = event.target.files[0];;
+                //this.sv1 = $("input[name='folderin']").val();
+                event.preventDefault();
+                alert(this.sv1);
+
+                if(this.sv1.length != 0){
+                    this.folders.push(this.sv1);
+                    alert(this.sv1);
+                    var obj = document.getElementById('fileupload') ; 
+                    obj.outerHTML=obj.outerHTML;
+                }else{
+                    alert("请选择文件夹");
+                }
+                
                 //this.sv=''
+            },
+            fileclick: function() {
+                //alert(this.sv);
+                //this.sv2 = $("input[name='filein']").val();
+
+                if(this.sv2.length != 0){
+                    this.files.push(this.sv2);
+
+                    var obj = document.getElementById('fileupload') ; 
+                    obj.outerHTML=obj.outerHTML;
+                }else{
+                    alert("请选择文件");
+                }
+                
+            },
+            addComp(event) {
+                alert("A");
+                event.preventDefault();
+                let formData = new FormData();
+
+                alert("hh");
+                formData.append('name', this.name);
+                formData.append('version', this.version);
+                formData.append('size', this.size);
+                formData.append('describle', this.describle);
+                formData.append('componentfile', this.allArr);
+                console.log(this.allArr.length);
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+
+                this.$axios.post('components', formData, {
+                    config,
+                    auth: {
+                        username: 'admin',
+                        password: 'admin'
+                    }
+                }).then(function (res) {
+                    if (res.status === 2000) {
+                        /*这里做处理*/
+                    }
+                })
             }
-        }
-
-
-
 
     }
+}
+
 </script>
 <style>
     .uploadfile{
         border: 1px solid #cccccc;
         border-radius: 4px;
-        height: 200px;
+        min-height: 250px;
 
     }
 
@@ -150,7 +252,7 @@ Vue.component('todo-item', {
 
     .addli{
         
-        max-height: 130px;
+        max-height: 90px;
         overflow: auto;
     }
 </style>
